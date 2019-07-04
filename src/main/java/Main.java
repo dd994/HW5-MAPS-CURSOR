64bc165a136cfd2bc7b1ef56bd9d958b49822539
Dmytro Dzhunenko <ddzhunenko@gmail.com>
2019-07-04 10:53:22 +0300

    add accounting class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,10 +4,12 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
+import static main.java.accounting.LibraryAccounting.searchByDate;
+
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
+        
         Map<String, String> library = new HashMap<>();
         library.put("2019-07-02", "Valeriy Ananiev - Slidy na dorozi");
         library.put("2019-07-03", "Stephen King - Happiness");
@@ -19,21 +21,12 @@ public class Main {
         System.out.println("Welcome to library! For searching a book, please, enter the date in format 'year-mm-dd'");
         String date = scan.nextLine();
         searchByDate(library, date);
-        System.out.println("If you want to see all dates of your visiting - enter 'y', if you want exit - enter any  key");
+        System.out.println("If you want to see all dates of your visiting - enter 'y', if you want exit - press any key");
         String yes = scan.nextLine();
         if (yes.equals("y")) {
             System.out.println(library.keySet());
         } else {
             System.out.println("Have a nice day!");
-        }
-    }
-
-    public static void searchByDate(Map<String, String> book, String date) {
-        if (book.containsKey(date)) {
-            System.out.println("At this day you read:");
-            System.out.println(book.get(date));
-        } else {
-            System.out.println("You don't take a book in this date or incorrect date");
         }
 
     }
